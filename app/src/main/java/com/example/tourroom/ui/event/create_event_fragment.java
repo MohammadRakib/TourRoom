90ce4bb89ed0002a56546786d18ae13e1d137ab6
MohammadRakib <mohammadrakibzaman@gmail.com>
2020-04-05 22:04:34 +0600

    all layout navigation complete fix

---
+++ app/src/main/java/com/example/tourroom/ui/event/create_event_fragment.java
@@ -0,0 +1,39 @@
+package com.example.tourroom.ui.event;
+
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tourroom.R;
+
+public class create_event_fragment extends Fragment {
+
+    private CreateEventFragmentViewModel mViewModel;
+
+    public static create_event_fragment newInstance() {
+        return new create_event_fragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.create_event_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CreateEventFragmentViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
